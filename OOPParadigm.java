2c6eb00bc21642646dbc42bdba0a151c73e4e8fc
Anuththara.d <anuththarad37@gmail.com>
2026-01-02 07:58:26 +0530

    Add files via upload
    Programming Paradigms

---
+++ OOPParadigm.java
@@ -0,0 +1,7 @@
+public class OOPParadigm {
+
+    public static void main(String[] args) {
+        OOPParadigmStudent student = new OOPParadigmStudent("Anuththara", 23);
+        student.displayDetails();
+    }
+}
---
+++ OOPParadigmStudent.java
@@ -0,0 +1,14 @@
+public class OOPParadigmStudent {
+    private String name;
+    private int age;
+
+    public OOPParadigmStudent(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void displayDetails() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+}
